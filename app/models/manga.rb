238c15bf1da28b465bdaa818383e9a785d4083bc
Matthew Isliefson <misliefson@gmail.com>
2017-11-07 19:11:51 +0000

    Added Validations to Manga Model

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,4 +1,7 @@
 class Manga < ApplicationRecord
+	validates :title, :author,:description, presence:true
+	validates :stock_quantity, :inclusion => {:in => 0..100}
+
 	def self.search(search)
 		where("title LIKE ? ", "%#{search}%")
 	end
