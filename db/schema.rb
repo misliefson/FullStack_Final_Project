6412e171b5009bf87dd4faa83985eb4166b08959
Matthew Isliefson <misliefson@gmail.com>
2017-11-08 16:23:06 +0000

    added validations to category and customer models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106175241) do
+ActiveRecord::Schema.define(version: 20171108161619) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "address"
+    t.string "city"
+    t.string "country"
+    t.string "province"
+    t.string "postal_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "mangas", force: :cascade do |t|
     t.string "title"
