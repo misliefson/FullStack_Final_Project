1b0deacc1fab40d75bbdb7c541e7709a38270340
Matthew Isliefson <misliefson@gmail.com>
2017-11-21 17:14:35 +0000

    Added keyword search by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,11 @@ class CategoriesController < ApplicationController
 
   def index
   	@categories = Category.all
+  	if params[:search]
+      @categories = Category.search(params[:search]).order("created_at DESC")
+    else
+      @categories = Category.all.order("created_at DESC")
+    end
   	@manga = Manga.all
   end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
 	has_many :mangas
 
 	validates :name, presence:true
+
+	def self.search(search)
+		where("name LIKE ?","%#{search}%")
+	end
 end
