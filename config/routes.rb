1a9d086f4c2d590eb58b6f6ad1b356454b9a06f9
Matthew Isliefson <misliefson@gmail.com>
2017-11-21 17:51:21 +0000

    Added routing for contact page

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get '/' => 'mangas#index', as: 'index'
   get '/categories' => 'categories#index', as: 'categories'
   get '/about' => 'abouts#index', as: 'about_index'
+  get '/contact' => 'contacts#index', as: 'contact_index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
