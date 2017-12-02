4a0858365f67d2e1d15cc7e6ee4b70f2d92a9b0a
Matthew Isliefson <misliefson@gmail.com>
2017-12-02 18:46:07 +0000

    Completed Stripe functionality

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,26 @@
 class ChargesController < ApplicationController
   def new
+  	@amount = (session[:total]).to_f
   end
 
   def create
+    # Amount in cents
+    @amount = session[:total].to_i * 100
+
+    @customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    @charge = Stripe::Charge.create(
+      :customer    => @customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'cad'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
   end
 end
