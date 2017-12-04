5c448afe291959471421599cfe271b228b3f74f6
Matthew Isliefson <misliefson@gmail.com>
2017-12-04 14:30:55 +0000

    Added removing and editing image via activeadmin

--- app/admin/mangas.rb
+++ app/admin/mangas.rb
@@ -1,5 +1,23 @@
 ActiveAdmin.register Manga do
 
-permit_params :title, :author, :description, :stock_quantity, :category_id
+permit_params :title, :author, :description, :stock_quantity, :category_id, :image, image_attributes: [:_destroy]
 
+
+form do |f|
+	f.semantic_errors
+	f.inputs do 
+		f.input :title
+		f.input :author
+		f.input :description
+		f.input :stock_quantity
+		f.input :category_id
+		f.input :image, as: :file
+		if f.object.image.present?
+			f.semantic_fields_for :image_attributes do |image_fields|
+				image_fields.input :_destroy, as: :boolean, label: 'Delete'
+			end
+		end
+	end
+	f.actions
+end
 end
--- app/models/manga.rb
+++ app/models/manga.rb
@@ -4,16 +4,20 @@ class Manga < ApplicationRecord
 
 	has_attached_file :image, {  validate_media_type: false }
 	validates_attachment_content_type :image, content_type: /\Aimage/
-  # Validate filename
-  validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
-  # Explicitly do not validate
-  do_not_validate_attachment_file_type :image
+  	# Validate filename
+  	validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
+  	# Explicitly do not validate
+  	do_not_validate_attachment_file_type :image
 
 	validates :title, :author,:description, presence:true
 	validates :stock_quantity, :inclusion => {:in => 0..100}
 
 	def self.search(search)
 		where("title LIKE ? ", "%#{search}%")
+	end
+
+	def image_attributes=(attributes)
+		image.clear if has_destroy_flag?(attributes) && !image.dirty?
 	end
 
 end
